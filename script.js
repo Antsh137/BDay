2c33dbcf631cdc7ae1e49ecf793bae6e0a0229df
Antsh137 <118977417+Antsh137@users.noreply.github.com>
2025-10-15 14:21:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,86 @@ const random = (min, max) => (Math.random() * (max - min + 1) + min) | 0;
 const timestamp = () => new Date().getTime();
 
 const fireworkSound = document.getElementById("firework-sound");
-if (fireworkSound) fireworkSound.volume = 0.4;
+fireworkSound.volume = 0.5;
+
+class Birthday {
+  constructor() {
+    this.resize();
+    this.fireworks = [];
+    this.counter = 0;
+  }
+
+  resize() {
+    this.width = canvas.width = window.innerWidth * devicePixelRatio;
+    this.height = canvas.height = window.innerHeight * devicePixelRatio;
+    ctx.scale(devicePixelRatio, devicePixelRatio);
+
+    let center = (this.width / 2) / devicePixelRatio | 0;
+    this.spawnA = center - (center / 4) | 0;
+    this.spawnB = center + (center / 4) | 0;
+    this.spawnC = this.height * 0.1 / devicePixelRatio;
+    this.spawnD = this.height * 0.5 / devicePixelRatio;
+  }
+
+  onClick(evt) {
+    // Hide hint when user first interacts
+    const hint = document.getElementById("tap-hint");
+    if (hint && !hint.classList.contains("hidden")) {
+      hint.classList.add("hidden");
+    }
+
+    if (fireworkSound) {
+      fireworkSound.currentTime = 0;
+      fireworkSound.play().catch(() => {});
+    }
+
+    let touch = evt.touches ? evt.touches[0] : evt;
+    let x = touch.clientX;
+    let y = touch.clientY;
+    let count = random(3, 5);
+
+    for (let i = 0; i < count; i++) {
+      this.fireworks.push(
+        new Firework(
+          random(this.spawnA, this.spawnB),
+          this.height / devicePixelRatio,
+          x,
+          y,
+          random(0, 260),
+          random(30, 110)
+        )
+      );
+    }
+    this.counter = -1;
+  }
+
+  update(delta) {
+    ctx.globalCompositeOperation = "hard-light";
+    ctx.fillStyle = `rgba(20,20,20,${7 * delta})`;
+    ctx.fillRect(0, 0, this.width, this.height);
+
+    ctx.globalCompositeOperation = "lighter";
+    for (let firework of this.fireworks) firework.update(delta);
+
+    this.counter += delta * 3;
+    if (this.counter >= 1) {
+      this.fireworks.push(
+        new Firework(
+          random(this.spawnA, this.spawnB),
+          this.height / devicePixelRatio,
+          random(0, this.width / devicePixelRatio),
+          random(this.spawnC, this.spawnD),
+          random(0, 360),
+          random(30, 110)
+        )
+      );
+      this.counter = 0;
+    }
+
+    if (this.fireworks.length > 800)
+      this.fireworks = this.fireworks.filter(f => !f.dead);
+  }
+}
 
 class Firework {
   constructor(x, y, targetX, targetY, shade, offsprings) {
@@ -19,23 +98,22 @@ class Firework {
 
   update(delta) {
     if (this.dead) return;
-    const xDiff = this.targetX - this.x;
-    const yDiff = this.targetY - this.y;
+
+    let xDiff = this.targetX - this.x;
+    let yDiff = this.targetY - this.y;
 
     if (Math.abs(xDiff) > 3 || Math.abs(yDiff) > 3) {
       this.x += xDiff * 2 * delta;
       this.y += yDiff * 2 * delta;
       this.history.push({ x: this.x, y: this.y });
-      if (this.history.length > 10) this.history.shift();
+      if (this.history.length > 20) this.history.shift();
     } else {
       if (this.offsprings && !this.madeChilds) {
-        const babies = this.offsprings / 2;
+        let babies = this.offsprings / 2;
         for (let i = 0; i < babies; i++) {
-          const targetX = this.x + this.offsprings * Math.cos((PI2 * i) / babies);
-          const targetY = this.y + this.offsprings * Math.sin((PI2 * i) / babies);
-          birthday.fireworks.push(
-            new Firework(this.x, this.y, targetX, targetY, this.shade, 0)
-          );
+          let targetX = this.x + this.offsprings * Math.cos((PI2 * i) / babies) | 0;
+          let targetY = this.y + this.offsprings * Math.sin((PI2 * i) / babies) | 0;
+          birthday.fireworks.push(new Firework(this.x, this.y, targetX, targetY, this.shade, 0));
         }
       }
       this.madeChilds = true;
@@ -43,114 +121,36 @@ class Firework {
     }
 
     if (this.history.length === 0) this.dead = true;
-    else {
-      for (let i = 0; i < this.history.length; i++) {
-        const point = this.history[i];
+    else if (this.offsprings) {
+      for (let i = 0; this.history.length > i; i++) {
+        let point = this.history[i];
         ctx.beginPath();
-        ctx.fillStyle = `hsl(${this.shade},100%,${i + 30}%)`;
+        ctx.fillStyle = "hsl(" + this.shade + ",100%," + i + "%)";
         ctx.arc(point.x, point.y, 1, 0, PI2, false);
         ctx.fill();
       }
+    } else {
+      ctx.beginPath();
+      ctx.fillStyle = "hsl(" + this.shade + ",100%,50%)";
+      ctx.arc(this.x, this.y, 1, 0, PI2, false);
+      ctx.fill();
     }
   }
 }
 
-class Birthday {
-  constructor() {
-    this.resize();
-    this.fireworks = [];
-    this.counter = 0;
-  }
-
-  resize() {
-    this.width = canvas.width = window.innerWidth;
-    this.height = canvas.height = window.innerHeight;
-    const center = this.width / 2;
-    this.spawnA = center - center / 3;
-    this.spawnB = center + center / 3;
-    this.spawnC = this.height * 0.1;
-    this.spawnD = this.height * 0.5;
-  }
-
-  onClick(evt) {
-    if (fireworkSound) {
-      fireworkSound.currentTime = 0;
-      fireworkSound.play().catch(() => {});
-    }
-
-    const x = evt.clientX || evt.touches?.[0].pageX;
-    const y = evt.clientY || evt.touches?.[0].pageY;
-    const count = random(2, 4);
-
-    for (let i = 0; i < count; i++) {
-      this.fireworks.push(
-        new Firework(
-          random(this.spawnA, this.spawnB),
-          this.height,
-          x,
-          y,
-          random(0, 360),
-          random(15, 60)
-        )
-      );
-    }
-
-    this.counter = -1;
-  }
-
-  autoFire() {
-    const x = random(50, this.width - 50);
-    const y = random(this.spawnC, this.spawnD);
-    const count = random(1, 3);
-    for (let i = 0; i < count; i++) {
-      this.fireworks.push(
-        new Firework(
-          random(this.spawnA, this.spawnB),
-          this.height,
-          x,
-          y,
-          random(0, 360),
-          random(20, 70)
-        )
-      );
-    }
-  }
-
-  update(delta) {
-    ctx.globalCompositeOperation = "hard-light";
-    ctx.fillStyle = `rgba(0,0,0,${6 * delta})`;
-    ctx.fillRect(0, 0, this.width, this.height);
-
-    ctx.globalCompositeOperation = "lighter";
-    for (const f of this.fireworks) f.update(delta);
-
-    this.counter += delta * 3;
-    if (this.counter >= 1) {
-      this.autoFire();
-      this.counter = 0;
-    }
-
-    if (this.fireworks.length > 300)
-      this.fireworks = this.fireworks.filter(f => !f.dead);
-  }
-}
-
-const canvas = document.getElementById("birthday");
-const ctx = canvas.getContext("2d");
-const birthday = new Birthday();
+let canvas = document.getElementById("birthday");
+let ctx = canvas.getContext("2d");
 let then = timestamp();
+let birthday = new Birthday();
 
 window.onresize = () => birthday.resize();
-document.addEventListener("touchstart", e => birthday.onClick(e));
-document.addEventListener("click", e => birthday.onClick(e), { passive: true });
-
-// Auto-fire every 2–3 seconds (for passive display)
-setInterval(() => birthday.autoFire(), random(2000, 3000));
+document.addEventListener("click", e => birthday.onClick(e));
+document.addEventListener("touchstart", e => birthday.onClick(e), { passive: true });
 
 (function loop() {
   requestAnimationFrame(loop);
-  const now = timestamp();
-  const delta = (now - then) / 1000;
+  let now = timestamp();
+  let delta = (now - then) / 1000;
   then = now;
   birthday.update(delta);
 })();
